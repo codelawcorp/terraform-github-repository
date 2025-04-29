be042d8d3e8bcbeb6c0c0b26b78bb815a0feb6f8
Maksym Onyshchenko <devops@maxim.run>
2025-04-28 17:39:39 -0700

    feat: all possible github_repository attributes

--- main.tf
+++ main.tf
@@ -1,12 +1,64 @@
 resource "github_repository" "this" {
   name        = var.name
   description = var.description
+  visibility  = var.visibility
 
-  visibility = var.visibility
+  homepage_url = var.homepage_url
+  topics       = var.topics
+
+  has_issues    = var.has_issues
+  has_projects  = var.has_projects
+  has_wiki      = var.has_wiki
+  has_downloads = var.has_downloads
+
+  allow_merge_commit     = var.allow_merge_commit
+  allow_squash_merge     = var.allow_squash_merge
+  allow_rebase_merge     = var.allow_rebase_merge
+  delete_branch_on_merge = var.delete_branch_on_merge
+
+  is_template    = var.is_template
+  default_branch = var.default_branch
+  archived       = var.archived
 
   template {
     owner                = var.template.owner
     repository           = var.template.repository
     include_all_branches = var.template.include_all_branches
+  }
+
+  dynamic "pages" {
+    for_each = var.pages != null ? [var.pages] : []
+    content {
+      source {
+        branch = pages.value.source.branch
+        path   = pages.value.source.path
+      }
+    }
+  }
+
+  dynamic "security_and_analysis" {
+    for_each = var.security_and_analysis != null ? [var.security_and_analysis] : []
+    content {
+      dynamic "advanced_security" {
+        for_each = security_and_analysis.value.advanced_security != null ? [security_and_analysis.value.advanced_security] : []
+        content {
+          status = advanced_security.value.status
+        }
+      }
+
+      dynamic "secret_scanning" {
+        for_each = security_and_analysis.value.secret_scanning != null ? [security_and_analysis.value.secret_scanning] : []
+        content {
+          status = secret_scanning.value.status
+        }
+      }
+
+      dynamic "secret_scanning_push_protection" {
+        for_each = security_and_analysis.value.secret_scanning_push_protection != null ? [security_and_analysis.value.secret_scanning_push_protection] : []
+        content {
+          status = secret_scanning_push_protection.value.status
+        }
+      }
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -19,12 +19,117 @@ variable "template" {
   description = "Template configuration for the GitHub repository"
   type = object({
     owner                = string
-    repository          = string
+    repository           = string
     include_all_branches = bool
   })
   default = {
     owner                = ""
-    repository          = ""
+    repository           = ""
     include_all_branches = false
   }
+}
+
+variable "homepage_url" {
+  description = "URL of a page describing the project"
+  type        = string
+  default     = ""
+}
+
+variable "topics" {
+  description = "List of topics to add to the repository"
+  type        = list(string)
+  default     = []
+}
+
+variable "has_issues" {
+  description = "Set to true to enable the GitHub Issues features on the repository"
+  type        = bool
+  default     = true
+}
+
+variable "has_projects" {
+  description = "Set to true to enable the GitHub Projects features on the repository"
+  type        = bool
+  default     = false
+}
+
+variable "has_wiki" {
+  description = "Set to true to enable the GitHub Wiki features on the repository"
+  type        = bool
+  default     = false
+}
+
+variable "has_downloads" {
+  description = "Set to true to enable the GitHub Downloads features on the repository"
+  type        = bool
+  default     = false
+}
+
+variable "allow_merge_commit" {
+  description = "Set to false to disable merge commits on the repository"
+  type        = bool
+  default     = true
+}
+
+variable "allow_squash_merge" {
+  description = "Set to false to disable squash merges on the repository"
+  type        = bool
+  default     = true
+}
+
+variable "allow_rebase_merge" {
+  description = "Set to false to disable rebase merges on the repository"
+  type        = bool
+  default     = true
+}
+
+variable "delete_branch_on_merge" {
+  description = "Automatically delete head branch after a pull request is merged"
+  type        = bool
+  default     = false
+}
+
+variable "is_template" {
+  description = "Set to true to tell GitHub that this is a template repository"
+  type        = bool
+  default     = false
+}
+
+variable "default_branch" {
+  description = "The name of the default branch of the repository"
+  type        = string
+  default     = "main"
+}
+
+variable "archived" {
+  description = "Specifies if the repository should be archived"
+  type        = bool
+  default     = false
+}
+
+variable "pages" {
+  description = "The repository's GitHub Pages configuration"
+  type = object({
+    source = object({
+      branch = string
+      path   = string
+    })
+  })
+  default = null
+}
+
+variable "security_and_analysis" {
+  description = "Security and analysis features for the repository"
+  type = object({
+    advanced_security = object({
+      status = string
+    })
+    secret_scanning = object({
+      status = string
+    })
+    secret_scanning_push_protection = object({
+      status = string
+    })
+  })
+  default = null
 }
