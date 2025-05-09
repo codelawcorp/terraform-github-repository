b5e7d38bcbdf280bafe6db73362d3fb54e3dcb18
kadazusa <azusakadota@gmail.com>
2025-05-09 14:24:29 -0700

    fix: github_repository_autolink_reference #4
    Formatted tf files
    close #4

--- main.tf
+++ main.tf
@@ -330,10 +330,10 @@ resource "github_issue_labels" "this" {
 resource "github_repository_autolink_reference" "this" {
   for_each = { for reference in var.autolink_references : reference.key_prefix => reference }
 
-  repository = github_repository.this.name
+  repository          = github_repository.this.name
   key_prefix          = each.value.key_prefix
   target_url_template = each.value.target_url_template
-  is_alphanumeric = each.value.is_alphanumeric
+  is_alphanumeric     = each.value.is_alphanumeric
 }
 
 
