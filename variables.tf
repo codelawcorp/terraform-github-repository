5cc17d99bc185f055fd4423f745e6242bc991025
Max Putilin <max.putilin@outlook.com>
2025-10-28 21:35:23 -0700

    fix: grammar fix

--- variables.tf
+++ variables.tf
@@ -536,7 +536,7 @@ variable "rulesets" {
 }
 
 variable "bootstrap_me" {
-  description = "Set to `true` to init a git project in the current modulre directory and to add the newly created repository as an upstream."
+  description = "Set to `true` to init a git project in the current module directory and to add the newly created repository as an upstream."
   type        = bool
   default     = false
   nullable    = false
