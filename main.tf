0a562722c68c775e4dcaa019bf5a43057d064d88
Max Onyx <devops@maxim.run>
2025-11-25 20:19:57 -0800

    fix: renaming main to prod

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@
 # }
 
 locals {
-  default_branch = coalesce(try(github_branch_default.this[0].branch, null), try(data.github_repository.template[0].default_branch, null), "main")
+  # default_branch = coalesce(try(github_branch_default.this[0].branch, null), try(data.github_repository.template[0].default_branch, null), var.default_branch)
+  default_branch = var.default_branch
 }
 
 resource "github_repository" "this" {
@@ -115,15 +116,9 @@ data "github_repository" "this" {
   name = var.name
 }
 
-# output "github_repository" {
-#   value = data.github_repository.this
-# }
-
 
 resource "github_branch_default" "this" {
-  # if  `data.github_repository.this.default_branch` is empty means the repo was just created
-  count = try(data.github_repository.template[0].default_branch, null) != var.default_branch && try(data.github_repository.this.default_branch, null) != null ? 1 : 0
-  # count = try(data.github_repository.template[0].default_branch, "no template used") != var.default_branch &&coalesce(data.github_repository.this.default_branch, "main") != "main"  ? 1 : 0 # if  `data.github_repository.this.default_branc` means the repo was just created
+  count = try(data.github_repository.template[0].default_branch, null) != var.default_branch && coalesce(data.github_repository.this.default_branch, "main") != var.default_branch ? 1 : 0
 
   repository = github_repository.this.name
   branch     = var.default_branch
